d1e0d1c00df814db8cd0d02956356990ca4d97aa
luluHyggeWOW <1344085034@qq.com>
2023-08-09 16:13:09 +0800

    8/9

---
+++ src/pages/index/index.vue
@@ -0,0 +1,14 @@
+
+
+<template>
+  <view class="index">index</view>
+  <uni-card title="卡片">
+
+  </uni-card>
+</template>
+<script setup lang="ts">
+//
+</script>
+<style lang="scss">
+//
+</style>
