01005939c8d9425fac9feed05a9be7eafe929ced
luluHyggeWOW <1344085034@qq.com>
2023-08-14 09:54:29 +0800

    新建地址

---
+++ src/pagesMember/address-form/address-form.vue
@@ -0,0 +1,141 @@
+<template>
+  <view class="content">
+    <form>
+      <!-- 表单内容 -->
+      <view class="form-item">
+        <text class="label">收货人</text>
+        <input class="input" placeholder="请填写收货人姓名" v-model="form.receiver" />
+      </view>
+      <view class="form-item">
+        <text class="label">手机号码</text>
+        <input class="input" placeholder="请填写收货人手机号码" v-model="form.contact" />
+      </view>
+      <view class="form-item">
+        <text class="label">所在地区</text>
+        <picker @change="onRegionChange" class="picker" mode="region" :value="form.fullLocation.split(' ')">
+          <view v-if="form.fullLocation">{{form.fullLocation}}</view>
+          <view v-else class="placeholder">请选择省/市/区(县)</view>
+        </picker>
+      </view>
+      <view class="form-item">
+        <text class="label">详细地址</text>
+        <input class="input" placeholder="街道、楼牌号等信息" v-model="form.address" />
+      </view>
+      <view class="form-item">
+        <label class="label">设为默认地址</label>
+        <switch @change="onSwitchChange" class="switch" color="#27ba9b" :checked="form.isDefault === 1" />
+      </view>
+    </form>
+  </view>
+  <!-- 提交按钮 -->
+  <button @tap="onSubmit" class="button">保存并使用</button>
+</template>
+<script setup lang="ts">
+import { ref, defineProps } from 'vue'
+import { postMemberAddressAPI } from '@/services/address'
+// 表单数据
+const form = ref({
+  receiver: '', // 收货人
+  contact: '', // 联系方式
+  fullLocation: '', // 省市区(前端展示)
+  provinceCode: '', // 省份编码(后端参数)
+  cityCode: '', // 城市编码(后端参数)
+  countyCode: '', // 区/县编码(后端参数)
+  address: '', // 详细地址
+  isDefault: 0, // 默认地址，1为是，0为否
+})
+const props = defineProps<{ id: string }>()
+uni.setNavigationBarTitle({ title: props.id ? '修改地址' : '新建地址' })
+const onRegionChange: UniHelper.RegionPickerOnChange = (ev) => {
+  form.value.fullLocation = ev.detail.value.join(' ')
+  const [provinceCode, cityCode, countyCode] = ev.detail.code!
+  Object.assign(form.value, {
+    provinceCode,
+    cityCode,
+    countyCode,
+  })
+}
+const onSwitchChange: UniHelper.SwitchOnChange = (ev) => {
+  form.value.isDefault = ev.detail.value ? 1 : 0
+}
+const onSubmit = async () => {
+  await postMemberAddressAPI(form.value)
+  uni.showToast({ icon: 'success', title: '添加成功' })
+  setTimeout(() => {
+    uni.navigateBack()
+  }, 500)
+}
+</script>
+
+<style lang="scss">
+page {
+  background-color: #f4f4f4;
+}
+
+.content {
+  margin: 20rpx 20rpx 0;
+  padding: 0 20rpx;
+  border-radius: 10rpx;
+  background-color: #fff;
+
+  .form-item,
+  .uni-forms-item {
+    display: flex;
+    align-items: center;
+    min-height: 96rpx;
+    padding: 25rpx 10rpx;
+    background-color: #fff;
+    font-size: 28rpx;
+    border-bottom: 1rpx solid #ddd;
+    position: relative;
+    margin-bottom: 0;
+
+    // 调整 uni-forms 样式
+    .uni-forms-item__content {
+      display: flex;
+    }
+
+    .uni-forms-item__error {
+      margin-left: 200rpx;
+    }
+
+    &:last-child {
+      border: none;
+    }
+
+    .label {
+      width: 200rpx;
+      color: #333;
+    }
+
+    .input {
+      flex: 1;
+      display: block;
+      height: 46rpx;
+    }
+
+    .switch {
+      position: absolute;
+      right: -20rpx;
+      transform: scale(0.8);
+    }
+
+    .picker {
+      flex: 1;
+    }
+
+    .placeholder {
+      color: #808080;
+    }
+  }
+}
+
+.button {
+  height: 80rpx;
+  margin: 30rpx 20rpx;
+  color: #fff;
+  border-radius: 80rpx;
+  font-size: 30rpx;
+  background-color: #27ba9b;
+}
+</style>
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,55 +2,78 @@ import { useMemberStore } from '@/stores/modules/member'
 
 const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
 const memberStore = useMemberStore()
-export const httpInterceptor = {
+const httpInterceptor = {
+  // 拦截前触发
   invoke(options: UniApp.RequestOptions) {
+    // 1. 非 http 开头需拼接地址
     if (!options.url.startsWith('http')) {
       options.url = baseURL + options.url
     }
-    options.timeout = 5000
+    // 2. 请求超时
+    options.timeout = 10000
+    // 3. 添加小程序端请求头标识
     options.header = {
       ...options.header,
       'source-client': 'miniapp',
     }
-    let token = memberStore.profile?.token
+    // 4. 添加 token 请求头标识
+    const memberStore = useMemberStore()
+    const token = memberStore.profile?.token
     if (token) {
-      options.header.Authorizetion = token
+      options.header.Authorization = token
     }
-  }
+  },
 }
+
+// 拦截 request 请求
+uni.addInterceptor('request', httpInterceptor)
+// 拦截 uploadFile 文件上传
+uni.addInterceptor('uploadFile', httpInterceptor)
 interface Data<T> {
   code: string,
   msg: string,
   result: T
 }
-uni.addInterceptor('request', httpInterceptor)
-uni.addInterceptor('uploadFile', httpInterceptor)
+type Data<T> = {
+  code: string
+  msg: string
+  result: T
+}
+// 2.2 添加类型，支持泛型
 export const http = <T>(options: UniApp.RequestOptions) => {
+  // 1. 返回 Promise 对象
   return new Promise<Data<T>>((resolve, reject) => {
     uni.request({
       ...options,
+      // 响应成功
       success(res) {
+        // 状态码 2xx， axios 就是这样设计的
         if (res.statusCode >= 200 && res.statusCode < 300) {
+          // 2.1 提取核心数据 res.data
           resolve(res.data as Data<T>)
-        } else if (res.statusCode == 401) {
+        } else if (res.statusCode === 401) {
+          // 401错误  -> 清理用户信息，跳转到登录页
+          const memberStore = useMemberStore()
           memberStore.clearProfile()
           uni.navigateTo({ url: '/pages/login/login' })
           reject(res)
         } else {
+          // 其他错误 -> 根据后端错误信息轻提示
           uni.showToast({
             icon: 'none',
-            title: (res.data as Data<T>).msg || '请求错误'
+            title: (res.data as Data<T>).msg || '请求错误',
           })
+          reject(res)
         }
-
       },
+      // 响应失败
       fail(err) {
         uni.showToast({
           icon: 'none',
-          title: '网络错误'
+          title: '网络错误，换个网络试试',
         })
         reject(err)
-      }
+      },
     })
   })
 }
