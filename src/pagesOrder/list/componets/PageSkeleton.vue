357fd9a3f60f22761f4b1972e4e3faa30f6bc9c6
luluHyggeWOW <1344085034@qq.com>
2023-08-16 15:48:26 +0800

    订单列表渲染

---
+++ src/pagesOrder/list/componets/PageSkeleton.vue
@@ -0,0 +1,446 @@
+<!--
+此文件为开发者工具生成，生成时间: 2023/8/15下午4:30:03
+使用方法：
+在 E:\uni-app\VSCodeProjects\heima-shop\dist\dev\mp-weixin\pagesOrder\deatil\deatil.wxml 引入模板
+
+```
+<import src="deatil.skeleton.wxml"/>
+<template is="skeleton" wx:if="{{loading}}" />
+```
+
+在 E:\uni-app\VSCodeProjects\heima-shop\dist\dev\mp-weixin\pagesOrder\deatil\deatil.wxss 中引入样式
+```
+@import "./deatil.skeleton.wxss";
+```
+
+更多详细信息可以参考文档：https://developers.weixin.qq.com/miniprogram/dev/devtools/skeleton.html
+-->
+<template name="skeleton">
+  <view class="sk-container">
+    <view class="navbar" style="padding-top:47px">
+      <view class="wrap">
+        <navigator class="back icon-home sk-pseudo sk-pseudo-circle" open-type="switchTab"></navigator>
+        <view class="title sk-transparent sk-text-14-2857-132 sk-text">订单详情</view>
+      </view>
+    </view>
+    <scroll-view scroll-y="true" class="viewport" id="scroller" enable-back-to-top="true">
+      <view class="overview sk-image" style="padding-top:67px">
+        <view class="status icon-clock sk-transparent sk-text-0-0000-570 sk-text sk-pseudo sk-pseudo-circle">等待付款</view>
+        <view class="tips sk-transparent">
+          <text class="money sk-transparent sk-text-0-0000-103 sk-text">应付金额: ¥ 99.00</text>
+          <text class="time sk-transparent sk-text-0-0000-512 sk-text">支付剩余</text> 00 时 29 分 59 秒
+        </view>
+        <view class="button sk-transparent sk-text-28-1250-226 sk-text" style="background-position-x: 50%;">去支付</view>
+      </view>
+      <view class="shipment">
+        <view class="item sk-image">
+          <view class="message sk-transparent sk-text-14-2857-451 sk-text"> 您已在广州市天河区黑马程序员完成取件，感谢使用菜鸟驿站，期待再次为您服务。
+          </view>
+          <view class="date sk-transparent sk-text-14-2857-662 sk-text"> 2023-04-14 13:14:20 </view>
+        </view>
+        <view class="locate sk-image">
+          <view class="user sk-transparent sk-text-14-2857-49 sk-text"> 张三 13333333333 </view>
+          <view class="address sk-transparent sk-text-14-2857-961 sk-text"> 广东省 广州市 天河区 黑马程序员 </view>
+        </view>
+      </view>
+      <view class="goods">
+        <view class="item">
+          <navigator class="navigator" hover-class="none">
+            <image class="cover sk-image"></image>
+            <view class="meta">
+              <view class="name ellipsis sk-transparent sk-text-14-2857-230 sk-text">ins风小碎花泡泡袖衬110-160cm</view>
+              <view class="type sk-transparent sk-text-22-2222-532 sk-text">藏青小花， 130</view>
+              <view class="price">
+                <view class="actual">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="sk-transparent sk-text-14-2857-880 sk-text">99.00</text>
+                </view>
+              </view>
+              <view class="quantity sk-transparent sk-opacity">x1</view>
+            </view>
+          </navigator>
+          <navigator class="navigator" hover-class="none">
+            <image class="cover sk-image"></image>
+            <view class="meta">
+              <view class="name ellipsis sk-transparent sk-text-14-2857-895 sk-text">ins风小碎花泡泡袖衬110-160cm</view>
+              <view class="type sk-transparent sk-text-22-2222-581 sk-text">藏青小花， 130</view>
+              <view class="price">
+                <view class="actual">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="sk-transparent sk-text-14-2857-228 sk-text">99.00</text>
+                </view>
+              </view>
+              <view class="quantity sk-transparent sk-opacity">x1</view>
+            </view>
+          </navigator>
+          <view class="action">
+            <view class="button primary sk-transparent sk-text-28-3333-473 sk-text" style="background-position-x: 50%;">
+              申请售后</view>
+            <navigator class="button sk-transparent sk-text-28-3333-335 sk-text" style="background-position-x: 50%;">
+              去评价 </navigator>
+          </view>
+        </view>
+        <view class="total">
+          <view class="row">
+            <view class="text sk-transparent sk-text-0-0000-729 sk-text">商品总价: </view>
+            <view class="symbol sk-transparent sk-text-0-0000-472 sk-text sk-pseudo sk-pseudo-circle">99.00</view>
+          </view>
+          <view class="row">
+            <view class="text sk-transparent sk-text-0-0000-587 sk-text">运费: </view>
+            <view class="symbol sk-transparent sk-text-0-0000-687 sk-text sk-pseudo sk-pseudo-circle">10.00</view>
+          </view>
+          <view class="row">
+            <view class="text sk-transparent sk-text-0-0000-537 sk-text">应付金额: </view>
+            <view class="symbol primary sk-transparent sk-text-0-0000-216 sk-text sk-pseudo sk-pseudo-circle">109.00
+            </view>
+          </view>
+        </view>
+      </view>
+      <view class="detail">
+        <view class="title sk-transparent sk-text-0-0000-931 sk-text">订单信息</view>
+        <view class="row">
+          <view class="item sk-transparent"> 订单编号: 1691361759169875970
+            <text class="copy sk-transparent sk-text-0-0000-88 sk-text">复制</text>
+          </view>
+          <view class="item sk-transparent sk-text-0-0000-789 sk-text">下单时间: 2023-04-14 13:14:20</view>
+        </view>
+      </view>
+      <view class="toolbar" style="padding-bottom:34px">
+        <view class="button primary sk-transparent sk-text-14-2857-212 sk-text"> 去支付 </view>
+        <view class="button sk-transparent sk-text-14-2857-245 sk-text"> 取消订单 </view>
+      </view>
+    </scroll-view>
+  </view>
+</template>
+<style>
+/*
+此文件为开发者工具生成，生成时间: 2023/8/15下午4:30:04
+
+在 E:\uni-app\VSCodeProjects\heima-shop\dist\dev\mp-weixin\pagesOrder\deatil\deatil.wxss 中引入样式
+```
+@import "./deatil.skeleton.wxss";
+```
+
+更多详细信息可以参考文档：https://developers.weixin.qq.com/miniprogram/dev/devtools/skeleton.html
+*/
+.sk-transparent {
+  color: transparent !important;
+}
+.sk-text-14-2857-132 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 44.8rpx;
+  position: relative !important;
+}
+.sk-text {
+  background-origin: content-box !important;
+  background-clip: content-box !important;
+  background-color: transparent !important;
+  color: transparent !important;
+  background-repeat: repeat-y !important;
+}
+.sk-text-0-0000-570 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-103 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 28rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-512 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 28rpx;
+  position: relative !important;
+}
+.sk-text-28-1250-226 {
+  background-image: linear-gradient(
+    transparent 28.125%,
+    #eeeeee 0%,
+    #eeeeee 71.875%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 64rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-451 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-662 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-49 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-961 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-230 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-22-2222-532 {
+  background-image: linear-gradient(
+    transparent 22.2222%,
+    #eeeeee 0%,
+    #eeeeee 77.7778%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 43.2rpx;
+  position: relative !important;
+}
+.sk-opacity {
+  opacity: 0 !important;
+}
+.sk-text-14-2857-880 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-895 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-22-2222-581 {
+  background-image: linear-gradient(
+    transparent 22.2222%,
+    #eeeeee 0%,
+    #eeeeee 77.7778%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 43.2rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-228 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-text-28-3333-473 {
+  background-image: linear-gradient(
+    transparent 28.3333%,
+    #eeeeee 0%,
+    #eeeeee 71.6667%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 60rpx;
+  position: relative !important;
+}
+.sk-text-28-3333-335 {
+  background-image: linear-gradient(
+    transparent 28.3333%,
+    #eeeeee 0%,
+    #eeeeee 71.6667%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 60rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-729 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-472 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-587 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-687 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-537 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-216 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-931 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 30rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-88 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 20rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-789 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-212 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-245 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-image {
+  background: #efefef !important;
+}
+.sk-pseudo::before,
+.sk-pseudo::after {
+  background: #efefef !important;
+  background-image: none !important;
+  color: transparent !important;
+  border-color: transparent !important;
+}
+.sk-pseudo-rect::before,
+.sk-pseudo-rect::after {
+  border-radius: 0 !important;
+}
+.sk-pseudo-circle::before,
+.sk-pseudo-circle::after {
+  border-radius: 50% !important;
+}
+.sk-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: transparent;
+}
+</style>
--- src/services/order.ts
+++ src/services/order.ts
@@ -1,4 +1,4 @@
-import type { OrderCreateParams, OrderLogisticResult, OrderPreResult, OrderResult } from '@/types/order'
+import type { OrderCreateParams, OrderListParams, OrderListResult, OrderLogisticResult, OrderPreResult, OrderResult } from '@/types/order'
 import { http } from '@/utils/http'
 /**
  * 填写订单-获取预付订单
@@ -91,3 +91,15 @@ export const deleteMemberOrderAPI = (data: { ids: string[] }) => {
     data,
   })
 }
+/**
+ * 获取订单列表
+ * @param data orderState 订单状态
+ */
+export const getMemberOrderAPI = (data: OrderListParams) => {
+  return http<OrderListResult>({
+    method: 'GET',
+    url: `/member/order`,
+    data,
+  })
+}
+
